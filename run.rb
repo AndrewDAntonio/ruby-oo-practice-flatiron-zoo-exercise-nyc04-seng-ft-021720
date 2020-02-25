e6845a8f103ab8383987dd3d8936aac4880e2210
AndrewDAntonio <github email address>
2020-02-25 11:58:01 -0500

    Complete

--- run.rb
+++ run.rb
@@ -3,8 +3,14 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+bronx = Zoo.new("Bronx Zoo", "NYC")
+sea_world = Zoo.new("Sea World", "San Diego")
+alex = Animal.new("Alex", "lion", 200, bronx)
+marty = Animal.new("Marty", "zebra", 150, bronx)
+simba = Animal.new("Simba", "lion", 250, bronx)
+shamoo = Animal.new("Shamoo", "whale", 700, sea_world)
 
+p bronx.find_by_species("zebra")
 
 binding.pry
 puts "done"
